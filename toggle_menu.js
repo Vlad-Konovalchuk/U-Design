124419133e499add8cca0adfca20f2760fc2fef8
Vlad Konovalchuk <berlitio600@gmail.com>
2018-04-13 11:33:37 +0300

    First commit

---
+++ toggle_menu.js
@@ -0,0 +1,21 @@
+// menu.onclick = function menuad() {
+//     var x = document.getElementById('myTopnav');
+//
+//     if (x.className === "header_navbar"){
+//         x.className += " responsive"
+//     } else {
+//         x.className = "header_navbar"
+//     }
+// }
+
+window.onload = funtion() {
+
+        document.querySelector('#menu').onclick = menuShow;
+        document.querySelector('#menu').onclick = menuHide;
+
+
+
+
+
+
+}
